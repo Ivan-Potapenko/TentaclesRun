d01ce9e55fbbfcd363488bb3ec761ba8f1e7a9ed
Ivan Potapenko <potapenko@panzerdog.com>
2022-01-09 23:34:11 +0200

    change TabletsGenerator

--- Tentacles/Assets/Scripts/Game/CthulhuController.cs
+++ Tentacles/Assets/Scripts/Game/CthulhuController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 namespace Game {
 
-    public class CthulhuControler : MonoBehaviour {
+    public class CthulhuController : MonoBehaviour {
 
         [SerializeField]
         private Cthulhu _cthulhu;
--- Tentacles/Assets/Scripts/Game/TabletsGenerator.cs
+++ Tentacles/Assets/Scripts/Game/TabletsGenerator.cs
@@ -19,12 +19,20 @@ namespace Game {
         [SerializeField]
         private List<SpawnZone> _spawnZones;
 
+        private float _spawnZoneRadiusSum = 0;
+
         [Serializable]
         private struct SpawnZone {
             public Vector2 center;
             public float radius;
         }
 
+        private void Start() {
+            _spawnZoneRadiusSum = 0;
+            foreach (var zone in _spawnZones) {
+                _spawnZoneRadiusSum += zone.radius;
+            }
+        }
 
         private void OnDrawGizmos() {
             if (!_showGizmos) {
@@ -46,17 +54,37 @@ namespace Game {
         }
 
         private IEnumerator SpawnTabletsCoroutine() {
+
             while (true) {
+                var position = GetRandomVectorInZone();
+                Instantiate(_tablets, position, Quaternion.identity);
+
                 var randomWaitOffset = UnityEngine.Random.Range(-_timeBetwinSpawnTablets / 2, _timeBetwinSpawnTablets / 2);
                 yield return new WaitForSeconds(_timeBetwinSpawnTablets + randomWaitOffset);
-                var randomSpawnZone = _spawnZones[UnityEngine.Random.Range(0, _spawnZones.Count)];
-                var theta = UnityEngine.Random.Range(0, (float)(2 * Math.PI * randomSpawnZone.radius));
-                var r = UnityEngine.Random.Range(0, randomSpawnZone.radius);
-                var x = (float)(r * Math.Cos(theta) + randomSpawnZone.center.x);
-                var y = (float)(r * Math.Sin(theta) + randomSpawnZone.center.y);
-
-                Instantiate(_tablets, new Vector3(x, y, 0), Quaternion.identity);
             }
+        }
+
+        private Vector3 GetRandomVectorInZone() {
+            var randomSpawnZone = GetRandoSpawnZone();
+            var theta = UnityEngine.Random.Range(0, (float)(2 * Math.PI * randomSpawnZone.radius));
+            var r = UnityEngine.Random.Range(0, randomSpawnZone.radius);
+            var x = (float)(r * Math.Cos(theta) + randomSpawnZone.center.x);
+            var y = (float)(r * Math.Sin(theta) + randomSpawnZone.center.y);
+            return new Vector3(x, y, 0);
+        }
+
+        private SpawnZone GetRandoSpawnZone() {
+            var radiusSum = 0f;
+            var randomZone = UnityEngine.Random.Range(0f, _spawnZoneRadiusSum);
+
+            for (int i = 0; i < _spawnZones.Count; i++) {
+                if (radiusSum < randomZone && (radiusSum + _spawnZones[i].radius) >= randomZone) {
+                    return _spawnZones[i];
+                }
+                radiusSum += _spawnZones[i].radius;
+            }
+
+            return _spawnZones[0];
         }
     }
 }
