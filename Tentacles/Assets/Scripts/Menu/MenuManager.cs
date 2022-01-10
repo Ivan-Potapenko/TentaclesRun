371167060fa1bd34d71c7b5ff65868839c7a4023
Ivan Potapenko <potapenko@panzerdog.com>
2022-01-10 02:56:06 +0200

    running late

---
+++ Tentacles/Assets/Scripts/Menu/MenuManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField]
+    private Button _playButton;
+
+    [SerializeField]
+    private Button _exitButton;
+
+    [SerializeField]
+    private Button _infoButton;
+
+    private void Start() {
+        _playButton.onClick.AddListener(LoadGameplayScene);
+        _exitButton.onClick.AddListener(Exit);
+        _infoButton.onClick.AddListener(LoadInfoScene);
+    }
+
+    private void LoadGameplayScene() {
+        SceneManager.LoadScene("GameplayScene");
+    }
+
+    private void LoadInfoScene() {
+        SceneManager.LoadScene("InfoScene");
+    }
+
+    private void Exit() {
+        Application.Quit();
+    }
+}
